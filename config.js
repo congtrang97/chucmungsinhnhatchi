fca0e5a83480bef8150a8fa6539181c97a5bb358
congtrang97 <ariesboy9x@gmail.com>
2024-09-26 22:07:50 +0700

    Add files via upload
    thiepsn

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Chị Dungg!';
+const giftUrl = 'https://www.facebook.com/Yung.meo95';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = '';
+const contentCard = 'Chúc chị có một ngày sinh nhật thật ý nghĩa! Chúc chị luôn luôn xinh đẹp, trẻ trung, vui vẻ, hạnh phúc, tràn đầy năng lượng, mọi điều viên mãn và thành công, gặp nhiều may mắn trong cuộc sống. You’re not getting older, you’re just getting wiser!';
+const giftImage = 'dunggg.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
